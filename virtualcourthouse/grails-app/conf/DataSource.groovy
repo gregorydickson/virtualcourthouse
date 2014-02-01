af4c3a88ff4ab580c0992476a5138e1cebdc9f3d
gregorydickson <home@Gregorys-MacBook-Air.local>
2014-02-01 11:21:06 -0600

    BootStrap environments

--- virtualcourthouse/grails-app/conf/DataSource.groovy
+++ virtualcourthouse/grails-app/conf/DataSource.groovy
@@ -1,22 +1,6 @@
 dataSource {
-    pooled = true
-    dbCreate = "update"
-    url = "jdbc:mysql://localhost/p1"
-    driverClassName = "com.mysql.jdbc.Driver"
-    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-    username = "land"
-    password = "0gzr4YjMKrsb2Ix"
-	    properties {
-        maxActive = 50
-        maxIdle = 25
-        minIdle = 5
-        initialSize = 5
-        minEvictableIdleTimeMillis = 60000
-        timeBetweenEvictionRunsMillis = 60000
-        maxWait = 10000
-        validationQuery = "/* ping */"
-    }
 }
+    
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -29,17 +13,67 @@ environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            
-        }
+		    pooled = true
+		    dbCreate = "update"
+		    url = "jdbc:mysql://localhost/p1"
+		    driverClassName = "com.mysql.jdbc.Driver"
+		    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		    username = "land"
+		    password = "0gzr4YjMKrsb2Ix"
+			    properties {
+		        maxActive = 50
+		        maxIdle = 25
+		        minIdle = 5
+		        initialSize = 5
+		        minEvictableIdleTimeMillis = 60000
+		        timeBetweenEvictionRunsMillis = 60000
+		        maxWait = 10000
+		        validationQuery = "/* ping */"
+        	}
+		}
     }
     test {
-        
-        
+		dataSource {
+	        dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+		    pooled = true
+		    dbCreate = "update"
+		    url = "jdbc:mysql://localhost/p1"
+		    driverClassName = "com.mysql.jdbc.Driver"
+		    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		    username = "land"
+		    password = "0gzr4YjMKrsb2Ix"
+			    properties {
+		        maxActive = 50
+		        maxIdle = 25
+		        minIdle = 5
+		        initialSize = 5
+		        minEvictableIdleTimeMillis = 60000
+		        timeBetweenEvictionRunsMillis = 60000
+		        maxWait = 10000
+		        validationQuery = "/* ping */"
+			}
+		}
     }
     production {
         dataSource {
-            
-            
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+		    pooled = true
+		    dbCreate = "update"
+		    url = "jdbc:mysql:///p1"
+		    driverClassName = "com.mysql.jdbc.Driver"
+		    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		    username = "myuse"
+		    password = "mypass"
+			    properties {
+		        maxActive = 50
+		        maxIdle = 25
+		        minIdle = 5
+		        initialSize = 5
+		        minEvictableIdleTimeMillis = 60000
+		        timeBetweenEvictionRunsMillis = 60000
+		        maxWait = 10000
+		        validationQuery = "/* ping */"
+            }
         }
     }
 }
