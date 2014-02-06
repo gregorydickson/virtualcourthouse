938f2194525cce33e486c00baf9a9eab9a02cb7e
gregorydickson <home@Gregorys-MacBook-Air.local>
2014-02-06 11:15:07 -0600

    commit

--- grails-app/services/virtualcourthouse/DocumentService.groovy
+++ grails-app/services/virtualcourthouse/DocumentService.groovy
@@ -5,16 +5,24 @@ import grails.transaction.Transactional
 @Transactional
 class DocumentService {
 
-    def createDocumentWithChildren( String questionText,  
-                                      List choices
-                                    ) 
+    def createDocumentWithChildren(List grantee, 
+									List grantor) 
     {
-        Question newQuestion = new Question( text : questionText )
-        choices.each {
-            Answer result = create(newQuestion, it.text, it.correct.equals("on") ? true : false)
-            newQuestion.addToAnswers(result)
-        }
-        newQuestion = persistQuestion(newQuestion)
-        return newQuestion
+        Document newDocument = new Document( InstrumentTypes : instrumentType,
+											bookNumber : bookNumber,
+											BookTypes : bookType,
+											Integer : pageNumber,
+											String : instrumentNumber,
+											Date : fileDate,
+											Date : instrumentDate)
+	
+
+		for (grantee in params.list('grantee') {
+			newDocument.grantee.add()
+		}
+		for (grantor in params.list('grantor') {
+			newDocument.grantor.add()
+		}
+
     }
 }
