cbc6487d3ecf83eb32990f221aac2363d12eeb57
Gregory Dickson <gregory.d.dickson@gmail.com>
2014-04-25 09:52:21 -0500

    updated datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -37,23 +37,55 @@ environments {
 		}
     }
 
-    production {
+    test {
         dataSource {
-           dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
 		    pooled = true
-		  	driverClassName = "com.mysql.jdbc.Driver"
-			  url = "jdbc:mysql://aa1ojksw5ci9npg.cta6bqnhgrir.us-west-2.rds.amazonaws.com/ebdb?user=land&password=0gzr4YjMKrsb2Ix"
-			  dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		    url = "jdbc:mysql://db1.test.rc-energy.net/p1"
+		    driverClassName = "com.mysql.jdbc.Driver"
+		    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
 		    username = "land"
 		    password = "0gzr4YjMKrsb2Ix"
 			    properties {
-		       		validationQuery = "SELECT 1"
-					   testOnBorrow = true
-					   testOnReturn = true
-					   testWhileIdle = true
-					   timeBetweenEvictionRunsMillis = 1000 * 60 * 30
-					   numTestsPerEvictionRun = 3
-					   minEvictableIdleTimeMillis = 1000 * 60 * 30
+		        maxActive = 50
+		        maxIdle = 25
+		        minIdle = 5
+		        initialSize = 5
+		        minEvictableIdleTimeMillis = 60000
+		        timeBetweenEvictionRunsMillis = 60000
+		        maxWait = 10000
+		        validationQuery = "/* ping */"
+				testOnBorrow=true
+			    testWhileIdle=true
+			    testOnReturn=true
+
+        	}
+			  
+		}
+	}
+
+	production {
+        dataSource {
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+		    pooled = true
+		    url = "jdbc:mysql://db1.prod.rc-energy.net/p1"
+		    driverClassName = "com.mysql.jdbc.Driver"
+		    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+		    username = "land"
+		    password = "0gzr4YjMKrsb2Ix"
+			    properties {
+		        maxActive = 50
+		        maxIdle = 25
+		        minIdle = 5
+		        initialSize = 5
+		        minEvictableIdleTimeMillis = 60000
+		        timeBetweenEvictionRunsMillis = 60000
+		        maxWait = 10000
+		        validationQuery = "/* ping */"
+				testOnBorrow=true
+			    testWhileIdle=true
+			    testOnReturn=true
+
         	}
 			  
 		}
